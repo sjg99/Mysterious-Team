feea023008c22f280f716dda6ccd2d66b91c4f21
sjg99 <38005341+sjg99@users.noreply.github.com>
2018-09-15 20:16:50 -0500

    Jara´s Push

--- ScotiaSuppliersAdmin/ScotiaSuppliersAdmin/Controllers/SuppliersController.cs
+++ ScotiaSuppliersAdmin/ScotiaSuppliersAdmin/Controllers/SuppliersController.cs
@@ -9,9 +9,22 @@ namespace ScotiaSuppliersAdmin.Controllers
     public class SuppliersController : Controller
     {
         // GET: Suppliers
+        private List<Models.supplier> LoadSuppliers()
+        {
+            List<Models.supplier> sls = new List<Models.supplier>();
+            using (Models.scotiasuppliersEntities db = new Models.scotiasuppliersEntities())
+            {
+                var GetSuppliers = db.suppliers.Where(x => x.SupplierId > 0);
+                foreach(var s in GetSuppliers)
+                {
+                    sls.Add(s);
+                }
+            }
+            return sls;
+        }
         public ActionResult Index()
         {
-            return View();
+            return View(LoadSuppliers());
         }
     }
 }
